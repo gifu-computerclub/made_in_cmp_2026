c9124449397a9dcda17ada8039a510a2855713ce
rangea100 <hinara1203@gmail.com>
2026-06-10 00:36:35 +0900

    セレクト画面追加

---
+++ titlemenu/scripts/title.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var shutter: TextureRect = %Shutter
+@onready var title_label: RichTextLabel = %TitleLabel
+
+func _ready() -> void:
+	#AudioManager.play_BGM("res://titlemenu/assets/audio/kosenwaribiki_game1.ogg",0.0,0.0,1.0,true,false)
+	pass
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("A"):
+		title_label.visible = false
+		var tween:Tween = create_tween()
+		tween.tween_property(shutter,"scale",Vector2(1,0.1),1).set_ease(Tween.EASE_OUT)
+		await tween.finished
+		SceneManager.change_scene("res://titlemenu/scenes/stage_select.tscn",{"color":Color(1.0, 1.0, 1.0, 1.0)})
