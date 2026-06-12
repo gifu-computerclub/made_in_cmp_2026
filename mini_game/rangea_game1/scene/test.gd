95a65667ccf961a83c4f8b3b500328038d0f1235
rangea100 <hinara1203@gmail.com>
2026-06-13 00:54:09 +0900

    startoverlayの設定反映を行えるようになった。

---
+++ mini_game/rangea_game1/scene/test.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+var toggle:bool = true
+
+func _ready() -> void:
+	#sprite_2d.position.x = 500
+	pass
+func _process(delta: float) -> void:
+	var diredtion:Vector2 = Vector2(1,0) if toggle else Vector2(-1,0)
+	sprite_2d.position += diredtion * 50
+	if sprite_2d.position.x > 980 or sprite_2d.position.x < 280:
+		toggle = !toggle
--- template/start_overlay/start_overlay.gd
+++ template/start_overlay/start_overlay.gd
@@ -1,27 +1,92 @@
+@tool
 extends Control
 
-@export var title_name:String = "タイトル"
-@export var title_color:Color = Color(0,0,1)
-@export var outline_color_black:bool = false
+@export var title_name:String = "タイトル":
+	set(value):
+		title_name = value
+		_set_viwe_editor()
+@export var title_color:Color = Color(0,0,1):
+	set(value):
+		title_color = value
+		_set_viwe_editor()
+@export var outline_color_black:bool = false:
+	set(value):
+		outline_color_black = value
+		_set_viwe_editor()
 @export_group("Title Slide")
-@export_custom(PROPERTY_HINT_GROUP_ENABLE,"") var title_slide:bool = true
-@export var after_position:Vector2 = Vector2(537,103)
-@export var after_size:int = 64
-@export var after_outline:int = 20
-@export var test:bool = false
+@export_custom(PROPERTY_HINT_GROUP_ENABLE,"") var title_slide:bool = true:
+	set(value):
+		title_slide = value
+		_set_viwe_editor()
+@export var after_position:Vector2 = Vector2(537,103):
+	set(value):
+		after_position = value
+		_set_viwe_editor()
+@export var after_size:int = 64:
+	set(value):
+		after_size = value
+		_set_viwe_editor()
+@export var after_outline:int = 20:
+	set(value):
+		after_outline = value
+		_set_viwe_editor()
+
+@export_group("")
+@export var test:bool = false:
+	set(value):
+		test = value
+		_test_view(value)
+@export var auto_start:bool = true
+@export var use_paused:bool = true
+
 @onready var countdown_label: Label = %CountdownLabel
 @onready var overlay: Control = $Overlay
 @onready var title_label: Label = $TitleLabel
-
-
+@onready var moved_label: Label = $MovedLabel
+var tite_size:int = 64
+var outline:int = 20
+var main:Node
 func _ready() -> void:
 	_set_viwe()
-	start_countdown()
+	if !Engine.is_editor_hint():
+		moved_label.visible = false
+		overlay.visible = true
+		title_label.visible = true
+		if use_paused:
+			_paused()
+		if auto_start:
+			start_countdown()
+			
 
+func _paused() -> void:
+	get_tree().paused = true
+	#main= get_tree().current_scene
+	#if main == self:
+		#print("mainはこのノードです。")
+	#else:
+		#main.set_process(false)
+		#main.set_physics_process(false)
 func _set_viwe() -> void:
 	title_label.text = title_name + "!!!!"
 	title_label.add_theme_color_override("font_color",title_color)
+	title_label.add_theme_color_override("font_outline_color",Color(0,0,0)if outline_color_black else Color(1,1,1))
+	if Engine.is_editor_hint():
+		moved_label.text = title_name + "!!!!"
+		moved_label.add_theme_color_override("font_color",title_color)
+		moved_label.add_theme_color_override("font_outline_color",Color(0,0,0)if outline_color_black else Color(1,1,1))
+		moved_label.visible = title_slide
+		moved_label.position = after_position
+		moved_label.add_theme_font_size_override("font_size",after_size)
+		moved_label.add_theme_constant_override("outline_size",after_outline)
+func _set_viwe_editor() -> void:
+	if Engine.is_editor_hint():
+		_set_viwe()
 
+func _test_view(value:bool) -> void:
+	if Engine.is_editor_hint():
+		overlay.visible = !value
+		title_label.visible = !value
+		moved_label.modulate.a = 1 if value else 0.5
 func start_countdown()-> void:
 	visible = true
 	AudioManager.play_BGM("res://titlemenu/assets/audio/kosenwaribiki_game1.ogg",0,0,1,true)
@@ -32,13 +97,17 @@ func start_countdown()-> void:
 	
 	overlay.visible = false
 	if title_slide:
-		var tween = create_tween()
+		var tween = create_tween().set_parallel()
 		tween.tween_property(title_label,"position",after_position,0.1).set_ease(Tween.EASE_OUT)
-		tween.parallel().tween_property(self,"tite_size",after_size,0.1)
-		tween.parallel().tween_property(self,"outline",after_outline,0.1)
+		tween.tween_property(self,"tite_size",after_size,0.1).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self,"outline",after_outline,0.1).set_ease(Tween.EASE_OUT)
 		await tween.finished
 	else:
 		title_label.visible = false
+	get_tree().paused = false
+func _process(delta: float) -> void:
+	title_label.add_theme_font_size_override("font_size",tite_size)
+	title_label.add_theme_constant_override("outline_size",outline)
 
 func show_countdown(text: String,color: Color) -> void:
 	countdown_label.text = text
